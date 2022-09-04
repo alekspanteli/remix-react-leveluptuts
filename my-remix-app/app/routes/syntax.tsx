fa6ee3c567e53c982307d3f7864fc2ff71ed6167
alekspanteli <alekspanteli@rambler.ru>
2022-09-04 20:58:14 +0000

    Update 2 files

--- my-remix-app/app/routes/syntax.tsx
+++ my-remix-app/app/routes/syntax.tsx
@@ -1,3 +1,21 @@
-export default function Index() {
-  return <h1>Syntax Podcast</h1>;
+import { Link, Outlet } from "@remix-run/react";
+
+export default function () {
+  return (
+    <>
+      <section>
+        <h1>Syntax Podcast</h1>
+      </section>
+      <aside>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/syntax/episode-1">Episode 1</Link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <Outlet />
+    </>
+  );
 }
