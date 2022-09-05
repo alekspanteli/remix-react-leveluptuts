67a3cca04103e3488776cf59b67b4aef590d6fa7
alekspanteli <alekspanteli@rambler.ru>
2022-09-05 07:18:39 +0000

    Update 3 files

--- my-remix-app/app/routes/syntax.tsx
+++ my-remix-app/app/routes/syntax.tsx
@@ -1,17 +1,41 @@
-import { Link, Outlet } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+
+export let loader = () => {
+  return {
+    podcastName: "Syntax podcast",
+    episodes: [
+      {
+        title: "Episode 1",
+        link: "episode-1",
+      },
+      {
+        title: "Episode 2",
+        link: "episode-2",
+      },
+      {
+        title: "Episode 3",
+        link: "episode-3",
+      },
+    ],
+  };
+};
 
 export default function () {
+  let { podcastName, episodes } = useLoaderData();
+
   return (
     <>
       <section>
-        <h1>Syntax Podcast</h1>
+        <h1>{podcastName}</h1>
       </section>
       <aside>
         <nav>
           <ul>
-            <li>
-              <Link to="/syntax/episode-1">Episode 1</Link>
-            </li>
+            {episodes.map((episode: any) => (
+              <li key={episode.link}>
+                <Link to={`/syntax/${episode.link}`}>{episode.title}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </aside>
